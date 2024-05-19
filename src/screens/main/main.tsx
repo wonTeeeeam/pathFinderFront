412c6f74085f8b13c7f75f92bc48ce5c4fb1ff35
한원석 <67740521+wonseokHan@users.noreply.github.com>
2024-05-19 16:09:56 +0900

    랭킹 페이지 중간 저장

--- src/screens/main/main.tsx
+++ src/screens/main/main.tsx
@@ -1,17 +1,40 @@
 import React, {useState} from 'react';
 import {Avatar, Button, Card, Text} from 'react-native-paper';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 function MainScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Card>
-        <Card.Title title="Card Title" subtitle="Card Subtitle" />
+        <Card.Title title="Lorem Ipsum" />
+        <Card.Cover
+          source={{uri: 'https://picsum.photos/700'}}
+          style={{
+            marginLeft: 7,
+            marginRight: 7,
+          }}
+        />
         <Card.Content>
-          <Text variant="titleLarge">Card title</Text>
-          <Text variant="bodyMedium">Card content</Text>
+          <Text variant="bodyMedium">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Text>
         </Card.Content>
-        <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
         <Card.Actions>
           <Button compact={true}>Cancel</Button>
           <Button compact={true} mode="contained-tonal">
--- src/screens/ranking/Ranking.tsx
+++ src/screens/ranking/Ranking.tsx
@@ -1,20 +1,122 @@
-import {useState} from 'react';
-import {Text, Button} from 'react-native-paper';
-import {View, Alert} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {DataTable} from 'react-native-paper';
+import {View, StyleSheet, Text, ScrollView, Image} from 'react-native';
+import {black} from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 
-function RankingScreen({navigation}) {
-  const items = useState([]);
-  const fn1 = () => {
-    navigation.goBack();
-  };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+function RankingScreen() {
+  const [numberOfItemsPerPageList] = useState([2, 3, 4]);
+
+  const [items] = useState([
+    {
+      key: 1,
+      name: 'Cupcake',
+      calories: 356,
+      fat: 16,
+    },
+    {
+      key: 2,
+      name: 'Eclair',
+      calories: 262,
+      fat: 16,
+    },
+    {
+      key: 3,
+      name: 'Frozen yogurt',
+      calories: 159,
+      fat: 6,
+    },
+    {
+      key: 4,
+      name: 'Gingerbread',
+      calories: 305,
+      fat: 3.7,
+    },
+    {
+      name: 'Gingerbread',
+    },
+    {},
+    {},
+    {},
+    {},
+    {},
+  ]);
 
   return (
-    <View>
-      <Text variant="headlineMedium">네비게이션1</Text>
-      <Button mode="outlined" onPress={() => fn1}>
-        hi
-      </Button>
-    </View>
+    <ScrollView>
+      <Text
+        variant="displayLarge"
+        style={{
+          marginLeft: 2,
+          marginBottom: 3,
+          marginTop: 5,
+        }}>
+        <Image
+          source={require('../../assets/images/crown_icon.png')}
+          style={{
+            width: 15,
+            height: 15,
+            marginRight: 2,
+          }}
+        />
+        랭킹 순위
+      </Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>순위</DataTable.Title>
+          <DataTable.Title>아이디</DataTable.Title>
+          <DataTable.Title numeric>점수</DataTable.Title>
+        </DataTable.Header>
+
+        {items.map((item, i) => (
+          <DataTable.Row key={i}>
+            <DataTable.Cell>
+              <Text
+                style={{
+                  color:
+                    i + 1 === 1
+                      ? '#ffd700'
+                      : i + 1 === 2
+                      ? '#c0c0c0'
+                      : i + 1 === 3
+                      ? '#bf9000'
+                      : null,
+                }}>
+                {i + 1}
+              </Text>
+            </DataTable.Cell>
+            <DataTable.Cell>{item.name}</DataTable.Cell>
+            <DataTable.Cell numeric>{item.fat}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              color: '#000000',
+              margin: 'auto 0',
+            }}>
+            &#8942;
+          </Text>
+          <DataTable.Row>
+            <DataTable.Cell>123</DataTable.Cell>
+            <DataTable.Cell>반원재</DataTable.Cell>
+            <DataTable.Cell>1</DataTable.Cell>
+          </DataTable.Row>
+        </View>
+      </DataTable>
+    </ScrollView>
   );
 }
 
